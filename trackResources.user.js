a2b45bb5afced45a1a744ff8f3b7aa7a4b791738
olivierasseb <olivierchou@telenet.be>
2016-10-04 21:18:46 +0200

    Res bags update
    fixed the issue of res bags not updating correctly in the update in a dirty, but working way

--- trackResources.user.js
+++ trackResources.user.js
@@ -165,7 +165,8 @@ function handleItemDrop(msg) {
 	if(printItemDrops) console.info(Date(), msg);
 
 	// TODO better method than this?
-	if(msg.indexOf('a resource bag') >= 0) tsResults.itemInfo.resourceBagDrop += 1;
+	if(msg.indexOf('resources bag') >= 0) tsResults.itemInfo.resourceBagDrop += 1;
+	else if(msg.indexOf('resource bags') >= 0) tsResults.itemInfo.resourceBagDrop += 1;
 	else if(msg.indexOf('a key') >= 0) tsResults.itemInfo.keyDrop += 1;
 	else if(msg.indexOf('a gem') >= 0) tsResults.itemInfo.gemDrop += 1;
 
